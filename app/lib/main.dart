ac7a9cfc5d8478bba3eeb45a6751bae62d4503cf
Jerry Li <jerli@xogrp.com>
2019-12-16 09:20:55 +0800

    Added icon to the list.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -29,17 +29,7 @@ class MyApp extends StatelessWidget {
 class StarterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+    return MaterialApp(home: RandomWords());
   }
 }
 
@@ -50,24 +40,54 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggesionList(),
+    );
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    final isSaved = _savedWordPairs.contains(wordPair);
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        isSaved ? Icons.favorite : Icons.favorite_border,
+        color: isSaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(isSaved) {
+            _savedWordPairs.remove(wordPair);
+          } else {
+            _savedWordPairs.add(wordPair);
+          }
+        });
+      },
+    );
   }
 
   Widget _buildSuggesionList() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
-        if(i.isOdd) {
+        if (i.isOdd) {
           return Divider();
         }
-        final index = i;
-        if(index  >= _suggestions.length) {
-          // _suggestions.addAll()
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
         }
+        return _buildRow(_suggestions[index]);
       },
     );
   }
--- note/get-started.md
+++ note/get-started.md
@@ -59,3 +59,47 @@ Debug mode trades performance for useful developer features such as hot reload a
 - The `Scaffold` widget, from the Material library, provides a default app bar, title, and a body property that holds the widget tree for the home screen.
 - A widget’s main job is to provide a build() method that describes how to display the widget in terms of other, lower level widgets.
 
+#### Step 2: Use an external package
+
+__Add the dependency__
+
+pubspec.yaml manages the asstes and dependencies for a Flutte app.
+
+```shell
+flutter pub get
+```
+
+Performing  get also auto-generates the pubspec.lock file with a list of all packages pulled into the project and their version numbers
+
+__Import it__
+
+```dart
+import 'package:english_words/english_words.dart';
+```
+
+#### Step 3: Add a Stateful widget
+
+Stateless widgets are immutable, meaning that their properties can’t change—all values are final.
+
+Stateful widgets maintain state that might change during the lifetime of the widget. Stateful widgets consist of 2 parts. The `StatefulWidget` create a instance of `State` __The `StatefulWidget` class is, itself, immutable, but the `State` class persists over the lifetime of the widget.__
+
+####  Step 4: Create an infinite scrolling ListView
+
+```dart
+final _suggestions = <WordPair>[]; //Prefixing an identifier with an underscore enforces privacy in the Dart language.
+```
+
+```dart
+final index = i ~/ 2; //The expression i ~/ 2 divides i by 2 and returns an integer result. 
+```
+
+### Write Your First Flutter App, part 2
+
+#### Add icons to the list
+
+`Icons` and `Colors` to define the Icon.
+
+#### Add interactivity
+
+
+
